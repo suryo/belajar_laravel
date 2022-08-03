0774e47f2653b677a185d6510c9cee9b36902a1e
suryo <suryoatm@gmail.com>
2022-08-03 11:25:26 +0700

    modified : routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,20 @@
 <?php
 
+/**
+ * For the brave souls who get this far: You are the chosen ones,
+ * the valiant knights of programming who toil away, without rest,
+ * fixing our most awful code. To you, true saviors, kings of men,
+ * I say this: never gonna give you up, never gonna let you down,
+ * never gonna run around and desert you. Never gonna make you cry,
+ * never gonna say goodbye. Never gonna tell a lie and hurt you.
+ */
+
+/**
+ * author : Suryo Atmojo <suryoatm@gmail.com>
+ * project : Belajar Laravel
+ * Start-date : 1-02-2021
+ */
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +32,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/produk', App\Http\Controllers\Api\ProdukController::class);
