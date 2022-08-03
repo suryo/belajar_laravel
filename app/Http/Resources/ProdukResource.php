549183446b826bc60101a050308ffdc56c38b826
suryo <suryoatm@gmail.com>
2022-08-03 11:24:33 +0700

    created : resource produk

---
+++ app/Http/Resources/ProdukResource.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * For the brave souls who get this far: You are the chosen ones,
+ * the valiant knights of programming who toil away, without rest,
+ * fixing our most awful code. To you, true saviors, kings of men,
+ * I say this: never gonna give you up, never gonna let you down,
+ * never gonna run around and desert you. Never gonna make you cry,
+ * never gonna say goodbye. Never gonna tell a lie and hurt you.
+ */
+
+/**
+ * author : Suryo Atmojo <suryoatm@gmail.com>
+ * project : Belajar Laravel
+ * Start-date : 1-02-2021
+ */
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProdukResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
