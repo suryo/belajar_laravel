a10afc0a206da1bcc046a1890e3e127fc866a2b2
suryo <suryoatm@gmail.com>
2022-09-16 10:57:27 +0700

    modified : fixed papi

--- app/Http/Controllers/PapiController.php
+++ app/Http/Controllers/PapiController.php
@@ -49,6 +49,27 @@ class PapiController extends Controller
             $p->aresult = 0;
             $p->nresult = 0;
 
+            $p->ganalisis = '';
+            $p->lanalisis = '';
+            $p->ianalisis = '';
+            $p->tanalisis = '';
+            $p->vanalisis = '';
+            $p->sanalisis = '';
+            $p->ranalisis = '';
+            $p->danalisis = '';
+            $p->canalisis = '';
+            $p->eanalisis = '';
+            $p->wanalisis = '';
+            $p->fanalisis = '';
+            $p->kanalisis = '';
+            $p->zanalisis = '';
+            $p->oanalisis = '';
+            $p->banalisis = '';
+            $p->xanalisis = '';
+            $p->panalisis = '';
+            $p->aanalisis = '';
+            $p->nanalisis = '';
+
             if ($p->jwb1 == 0) {
                 $p->gresult = $p->gresult + 1;
             } else {
@@ -971,6 +992,175 @@ class PapiController extends Controller
                 $p->nresult = $p->nresult + 1;
             } else {
                 $p->nresult = $p->nresult + 0;
+            }
+            // ================================================================== G ==========================
+            if (($p->gresult >= 0) && ($p->gresult <= 4)) {
+                $p->ganalisis = 'Bekerja untuk kesenangan saja, bukan hasil optimal';
+            } else
+             if (($p->gresult >= 5) && ($p->gresult <= 9)) {
+                $p->ganalisis = 'Kemauan bekerja keras tinggi';
+            }
+            // ================================================================== L ==========================
+            if (($p->lresult >= 0) && ($p->lresult <= 4)) {
+                $p->lanalisis = 'Cenderung tidak aktif menggunakan orang lain dalam bekerja. ';
+            } else
+             if (($p->lresult >= 5) && ($p->lresult <= 9)) {
+                $p->lanalisis = 'Memproyeksikan diri sebagai pemimpin.';
+            }
+            // ================================================================== i ==========================
+            if (($p->iresult >= 0) && ($p->iresult <= 4)) {
+                $p->ianalisis = 'Berhati-hati dalam mengambil keputusan.';
+            } else
+             if (($p->iresult >= 5) && ($p->iresult <= 7)) {
+                $p->ianalisis = 'Berhati-hati dan lancar dalam mengambil keputusan.';
+            } else
+             if (($p->iresult >= 8) && ($p->iresult <= 9)) {
+                $p->ianalisis = 'Tidak ragu dalam mengambil keputusan';
+            }
+            // ================================================================== t ==========================
+            if (($p->tresult >= 0) && ($p->tresult <= 3)) {
+                $p->tanalisis = 'Melakukan sesuatu menurut kemauannya sendiri';
+            } else
+if (($p->tresult >= 4) && ($p->tresult <= 9)) {
+                $p->tanalisis = 'Aktif secara internal dan mental';
+            }
+            // ================================================================== v ==========================
+            if (($p->vresult >= 0) && ($p->vresult <= 4)) {
+                $p->vanalisis = 'Cenderung pasif';
+            } else
+if (($p->vresult >= 5) && ($p->vresult <= 9)) {
+                $p->vanalisis = 'Aktif secara fisik, cenderung sportif';
+            }
+
+            // ================================================================== S ==========================
+            if (($p->sresult >= 0) && ($p->sresult <= 5)) {
+                $p->sanalisis = 'Perhatian rendah terhadap hubungan sosial';
+            } else
+            if (($p->sresult >= 6) && ($p->sresult <= 9)) {
+                $p->sanalisis = 'Kepercayaan tinggi dalam hubungan sosial, suka interaksi sosial';
+            }
+            // ================================================================== R ==========================
+            if (($p->rresult >= 0) && ($p->rresult <= 4)) {
+                $p->ranalisis = 'Kurang perhatian, bersifat praktis';
+            } else
+            if (($p->rresult >= 5) && ($p->rresult <= 9)) {
+                $p->ranalisis = 'Nilai penalaran tergolong tinggi';
+            }
+
+            // ================================================================== D ==========================
+            if (($p->dresult >= 0) && ($p->dresult <= 3)) {
+                $p->danalisis = 'Tidak berminat bekerja detail';
+            } else
+              if (($p->dresult >= 4) && ($p->dresult <= 9)) {
+                $p->danalisis = 'Minat tinggi unbtuk bekerja detail';
+            }
+            // ================================================================== C ==========================
+            if (($p->cresult >= 0) && ($p->cresult <= 5)) {
+                $p->canalisis = 'Teratur tetapi tidak tergolong flexibel';
+            } else
+            if (($p->cresult >= 6) && ($p->cresult <= 9)) {
+                $p->canalisis = 'Keteraturan tinggi, cenderung kaku';
+            }
+            // ================================================================== E ==========================
+            if (($p->eresult >= 0) && ($p->eresult <= 3)) {
+                $p->eanalisis = 'Terbuka';
+            } else
+             if (($p->eresult >= 4) && ($p->eresult <= 6)) {
+                $p->eanalisis = 'Punya pendekatan emosional yang seimbang';
+            } else
+             if (($p->eresult >= 7) && ($p->eresult <= 9)) {
+                $p->eanalisis = 'Sangat normative, kebutuhan pengendalian diri yang berlebihan';
+            }
+            // ================================================================== W ==========================
+            if (($p->wresult >= 0) && ($p->wresult <= 3)) {
+                $p->wanalisis = 'Berorientasi pada tujuan, mandiri';
+            } else
+             if (($p->wresult >= 4) && ($p->wresult <= 5)) {
+                $p->wanalisis = 'Kebutuhan akan pengarahan dan harapan yang dirumuskan untuknya';
+            } else
+             if (($p->wresult >= 6) && ($p->wresult <= 9)) {
+                $p->wanalisis = 'Meningkatnya orientasi terhadap tugas dan membutuhkan instruksi yang jelas';
+            }
+            // ================================================================== F ==========================
+            if (($p->fresult >= 0) && ($p->fresult <= 3)) {
+                $p->fanalisis = 'Mengurus kepentingannya sendiri';
+            } else
+             if (($p->fresult >= 4) && ($p->fresult <= 5)) {
+                $p->fanalisis = 'Setia terhadap perusahaan';
+            } else
+             if (($p->fresult >= 6) && ($p->fresult <= 9)) {
+                $p->fanalisis = 'Bersikap setia dan membantu';
+            }
+            // ================================================================== K ========================== 
+            if (($p->kresult >= 0) && ($p->kresult <= 2)) {
+                $p->kanalisis = 'Menghindari masalah';
+            } else
+             if (($p->kresult >= 3) && ($p->kresult <= 4)) {
+                $p->kanalisis = 'Suka lingkungan tenang, menghindari konflik';
+            } else
+             if (($p->kresult == 5)) {
+                $p->kanalisis = 'Keras kepala';
+            } else
+             if (($p->kresult >= 6) && ($p->kresult <= 9)) {
+                $p->kanalisis = 'Agresif, cenderung defensive';
+            }
+            // ================================================================== Z ==========================
+            if (($p->zresult >= 0) && ($p->zresult <= 4)) {
+                $p->zanalisis = 'Tidak suka perubahan jika dipaksakan';
+            } else
+             if (($p->zresult >= 5) && ($p->zresult <= 9)) {
+                $p->zanalisis = 'Mudah menyesuaikan diri';
+            }
+            // ================================================================== O ==========================
+            if (($p->oresult >= 0) && ($p->oresult <= 4)) {
+                $p->oanalisis = 'Sadar akan hubungan perorangan, tapi tidak terlalu tergantung';
+            } else
+             if (($p->oresult >= 5) && ($p->oresult <= 9)) {
+                $p->oanalisis = 'Sangat tergantung, butuh penerimaan diri';
+            }
+            // ================================================================== B ==========================
+            if (($p->bresult >= 0) && ($p->bresult <= 3)) {
+                $p->banalisis = 'Selektif';
+            } else
+             if (($p->bresult >= 4) && ($p->bresult <= 5)) {
+                $p->banalisis = 'Butuh diterima, tapi tidak mudah dipengaruhi kelompok';
+            } else
+             if (($p->bresult >= 6) && ($p->bresult <= 9)) {
+                $p->banalisis = 'Butuh disukai dan diakui, mudah dipengaruhi';
+            }
+            // ================================================================== X ==========================
+            if (($p->xresult >= 0) && ($p->xresult <= 3)) {
+                $p->xanalisis = 'Rendah kati, tulus';
+            } else
+             if (($p->xresult >= 4) && ($p->xresult <= 5)) {
+                $p->xanalisis = 'Memiliki pola prilaku unik';
+            } else
+             if (($p->xresult >= 6) && ($p->xresult <= 9)) {
+                $p->xanalisis = 'Membutuhkan perhatian nyata';
+            }
+            // ================================================================== P ==========================
+            if (($p->presult >= 0) && ($p->presult <= 4)) {
+                $p->panalisis = 'Menurunnya keinginan untuk bertanggung jawab pada pekerjaan orang lain';
+            } else
+            if (($p->presult >= 5) && ($p->presult <= 9)) {
+                $p->panalisis = 'Kebutuhan untuk menerima tanggung jawab dari pekerjaan orang lain.';
+            }
+            // ================================================================== A ==========================
+            if (($p->aresult >= 0) && ($p->aresult <= 5)) {
+                $p->aanalisis = 'Tidak ada usaha lebih';
+            } else
+             if (($p->aresult >= 6) && ($p->aresult <= 9)) {
+                $p->aanalisis = 'Tujuan jelas, kebutuhan sukses dan ambisi tinggi';
+            }
+            // ================================================================== N ==========================
+            if (($p->nresult >= 0) && ($p->nresult <= 4)) {
+                $p->nanalisis = 'Menunda/menghindari pekerjaan';
+            } else
+            if (($p->nresult >= 5) && ($p->nresult <= 6)) {
+                $p->nanalisis = 'Cukup bertanggung jawab pada pekerjaan';
+            } else
+            if (($p->nresult >= 7) && ($p->nresult <= 9)) {
+                $p->nanalisis = 'Memiliki tanggung jawab tinggi.';
             }
 
             array_push($result, $p);
--- resources/views/papi.blade.php
+++ resources/views/papi.blade.php
@@ -9,54 +9,94 @@
 <body>
     {{--contoh memunculkan data from controler --}}
     {{-- id_pendaftaran :   {{$data[0]->no_pendaftaran}} --}}
-    <table>
+    <table border=1>
         <th>
             No Pendaftaran
         </th>
 
-          <th>gresult</th>
-             <th>lresult</th>
-             <th>iresult</th>
-             <th>tresult</th>
-             <th>vresult</th>
-             <th>sresult</th>
-             <th>rresult</th>
-             <th>dresult</th>
-             <th>cresult</th>
-             <th>eresult</th>
-             <th>wresult</th>
-             <th>fresult</th>
-             <th>kresult</th>
-             <th>zresult</th>
-             <th>oresult</th>
-             <th>bresult</th>
-             <th>xresult</th>
-             <th>presult</th>
-             <th>aresult</th>
-             <th>nresult</th>
+            <th>g result</th>
+            <th>g analisis</th>
+             <th>l result</th>
+             <th>l analisis</th>
+             <th>i result</th>
+             <th>i analisis</th>
+             <th>t result</th>
+             <th>t analisis</th>
+             <th>v result</th>
+             <th>v analisis</th>
+             <th>s result</th>
+             <th>s analisis</th>
+             <th>r result</th>
+             <th>r analisis</th>
+             <th>d result</th>
+             <th>d analisis</th>
+             <th>c result</th>
+             <th>c analisis</th>
+             <th>e result</th>
+             <th>e analisis</th>
+             <th>w result</th>
+             <th>w analisis</th>
+             <th>f result</th>
+             <th>f analisis</th>
+             <th>k result</th>
+             <th>k analisis</th>
+             <th>z result</th>
+             <th>z analisis</th>
+             <th>o result</th>
+             <th>o analisis</th>
+             <th>b result</th>
+             <th>b analisis</th>
+             <th>x result</th>
+             <th>x analisis</th>
+             <th>p result</th>
+             <th>p analisis</th>
+             <th>a result</th>
+             <th>a analisis</th>
+             <th>n result</th>
+             <th>n analisis</th>
          @forelse ($data as $papi)
                                 <tr>
                                     <td>{{ $papi->no_pendaftaran }}</td>
-                                  <td>{{$papi->gresult}}</td>
+             <td>{{$papi->gresult}}</td>
+             <td>{{$papi->ganalisis}}</td>
              <td>{{$papi->lresult}}</td>
+             <td>{{$papi->lanalisis}}</td>
              <td>{{$papi->iresult}}</td>
+             <td>{{$papi->ianalisis}}</td>
              <td>{{$papi->tresult}}</td>
+             <td>{{$papi->tanalisis}}</td>
              <td>{{$papi->vresult}}</td>
+             <td>{{$papi->vanalisis}}</td>
              <td>{{$papi->sresult}}</td>
+             <td>{{$papi->sanalisis}}</td>
              <td>{{$papi->rresult}}</td>
+             <td>{{$papi->ranalisis}}</td>
              <td>{{$papi->dresult}}</td>
+             <td>{{$papi->danalisis}}</td>
              <td>{{$papi->cresult}}</td>
+             <td>{{$papi->canalisis}}</td>
              <td>{{$papi->eresult}}</td>
+             <td>{{$papi->eanalisis}}</td>
              <td>{{$papi->wresult}}</td>
+             <td>{{$papi->wanalisis}}</td>
              <td>{{$papi->fresult}}</td>
+             <td>{{$papi->fanalisis}}</td>
              <td>{{$papi->kresult}}</td>
+             <td>{{$papi->kanalisis}}</td>
              <td>{{$papi->zresult}}</td>
+             <td>{{$papi->zanalisis}}</td>
              <td>{{$papi->oresult}}</td>
+             <td>{{$papi->oanalisis}}</td>
              <td>{{$papi->bresult}}</td>
+             <td>{{$papi->banalisis}}</td>
              <td>{{$papi->xresult}}</td>
+             <td>{{$papi->xanalisis}}</td>
              <td>{{$papi->presult}}</td>
+             <td>{{$papi->panalisis}}</td>
              <td>{{$papi->aresult}}</td>
+             <td>{{$papi->aanalisis}}</td>
              <td>{{$papi->nresult}}</td>
+             <td>{{$papi->nanalisis}}</td>
                                     <td class="text-center">
                                         {{-- <form onsubmit="return confirm('Apakah Anda Yakin ?');"
                                             action="{{ route('post.destroy', $papi->id) }}" method="POST">
