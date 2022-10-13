117197b1757b4a42e1fdda15682e19ae9f2b7e78
AnaFebriYanti <anafebriyanti844@gmail.com>
2022-10-13 20:00:30 +0700

    Belajar Hari ini

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\controllers\CobaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/coba',[CobaController::class,'index']);
