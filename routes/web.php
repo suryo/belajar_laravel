0dda23df6c293892381e96d335bc6c35530c09d1
suryo <suryoatm@gmail.com>
2022-10-13 19:52:32 +0700

    created new controller coba & route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CobaController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/coba', [CobaController::class, 'index']);
+Route::get('/ayam', [CobaController::class, 'keren']);
