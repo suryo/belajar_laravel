67af7255fe534c0c0a817cb7ffb54f66b214d8ea
FaisalRhdn <faisalrahma531@gmail.com>
2022-10-25 02:59:37 +0700

    test commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\testcontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('coba');
 });
