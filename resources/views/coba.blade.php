9b9214acb2b5299f7563961b97978d432bd0aefb
FaisalRhdn <faisalrahma531@gmail.com>
2022-11-09 23:53:48 +0700

    Materi 16 auth

--- resources/views/coba.blade.php
+++ resources/views/coba.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Coba</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
     <h1>ini ada di resources/views</h1>
     <a href="{{ route('home') }}" class="btn btn-md btn-secondary">home</a>
-    <a href="{{ route('coba') }}" class="btn btn-md btn-secondary">test</a>
+    <a href="{{ route('coba') }}" class="btn btn-md btn-secondary">coba</a>
     <br>
     isinya ini var adalah : {{$inivar}}
     <br>
