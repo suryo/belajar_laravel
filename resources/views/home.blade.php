cb6d2b3104e4d8c09ea7c4edaa9ec1bbde71aba4
FaisalRhdn <faisalrahma531@gmail.com>
2022-11-18 01:49:00 +0700

    UTS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Berita') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                <table class="table table-bordered">
+                    <thead class="table-dark">
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Judul</th>
+                        <th scope="col">Berita</th>
+                        <th scope="col">Tanggal Berita</th>
+                        <th scope="col">Status</th>
+                    </tr>
+                    </thead>
+                @foreach ($berita as $isi)
+                    <tbody>
+                    <tr>
+                        <th scope="row">{{$isi->id}}</th>
+                        <td>{{$isi->judul}}</td>
+                        <td>{{$isi->berita}}</td>
+                        <td>{{$isi->tanggal}}</td>
+                        <td>{{$isi->status}}</td>
+                    </tr>
+                    </tbody>
+                @endforeach
+                <style>
+                th{
+                text-align: center;
+                font-size: 20px;
+                vertical-align: middle;
+                }
+                </style>
+                </table>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
+                    
                 </div>
             </div>
         </div>
