97e44db1219e9a35de7e7231c7cc34ec1586f906
suryo <suryoatm@gmail.com>
2022-12-03 11:19:58 +0700

    fruitcake laravel cors

--- config/app.php
+++ config/app.php
@@ -161,7 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Barryvdh\Cors\ServiceProvider::class,
+
+
 
         /*
          * Package Service Providers...
